ac02c415f4a67d21e4717f3c86cfe17e343ba38e
GuilhermeSanchesRossi <guilherme.s.rossi@hotmail.com>
2022-03-03 19:12:58 -0300

    Exercicio 1

---
+++ exercícios/src/App.java
@@ -0,0 +1,10 @@
+public class Exercício 1 {
+    public static void main(String[] args) {
+        //Imprima todos os numeros de 150 a 300
+        int i;
+        for (i = 150; i <= 300; i++){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
