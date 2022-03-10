3715913a902f37d34c7c446619873c5c3633a036
GuilhermeSanchesRossi <guilherme.s.rossi@hotmail.com>
2022-03-10 13:01:36 -0300

    Caneta com diversos métodos agora

--- Aula-4-Intro_POO/src/App.java
+++ Aula-4-Intro_POO/src/App.java
@@ -2,10 +2,17 @@ public class App {
     public static void main(String[] args) {
         //Declara e instancia um objeto Caneta
         Caneta c1 = new Caneta();
-        c1.modelo = "Bic";
-        c1.cor = "Azul";
-        c1.carga = 100;
-        c1.ponta = 0.7;
-        System.out.println("Minha caneta" + "\n" + c1.modelo + "\n" + c1.cor + "\n" + c1.carga + "\n" + c1.ponta);
+        c1.iniciarCaneta("Bic", "Azul", 0.7);
+        Caneta c2 = new Caneta();
+        c2.iniciarCaneta("Stabillo", "Vermelha", 0.5);
+        
+        System.out.println("Minha caneta 1:\n" + c1.pegarDados());
+        System.out.println("Minha caneta 2:\n" + c2.pegarDados());
+
+        c1.escrever("Vai Corintia porra");
+        c2.escrever("parmera bi rebaixado sem mundial");
+
+        System.out.println("Minha caneta 1:\n" + c1.pegarDados());
+        System.out.println("Minha caneta 2:\n" + c2.pegarDados());
     }
 }
--- Aula-4-Intro_POO/src/Caneta.java
+++ Aula-4-Intro_POO/src/Caneta.java
@@ -1,12 +1,31 @@
 public class Caneta {
-    //Caracteristicas da caneta -  seus atributos
+    //Características da caneta -  seus atributos
     String modelo;
     String cor;
     double ponta;
     int carga;
+    final int CARGA_MAXIMA = 100; 
 
-    //Comportamentos da caneta - seus metodos
-    void escrever(String texto){
-        System.out.println(texto);
+    //Comportamentos da caneta - seus métodos
+    void escrever(String texto) {
+        for (int i = 0; i < texto.length(); i++){
+            if (carga > 0){
+                System.out.print(texto.charAt(i));
+                carga -= 1;
+            } else {
+                System.out.print("CANETA SEM CARGA!");
+            }
+        }    
+    }
+
+    void iniciarCaneta(String modelo, String cor, double ponta) {
+        this.modelo = modelo;
+        this.cor = cor;
+        this.ponta = ponta;
+        this.carga = CARGA_MAXIMA;
+    }
+
+    String pegarDados() {
+        return "\nModelo: " + modelo + "\nCor: " + cor + "\nPonta: " + ponta + "\nCarga: " + carga;
     }
 }
