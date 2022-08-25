cdbf0986c673dbc50840ee8424a3fbc5d6856091
GUILHERME SANCHES ROSSI <19.02404-5@maua.br>
2022-08-25 12:32:01 -0300

    Criação de diretório dados + tratamento de exceptions

--- manipular_arquivos/main.py
+++ manipular_arquivos/main.py
@@ -2,15 +2,27 @@
 
 #LEMBRAR DE SALVAR O ARQUIVO MAIN ANTES DE RODAR/ COMITAR   
 
-arquivo = open("dados.txt", "w")
-continuar = True
+try:
+    arquivo = open("dados/dados.txt", "a")
+    continuar = True
 
-while continuar:
-    equipe = input("Equipe (vazio para sair):")
-    #TODA STRING VAZIA É FALSE
-    #portanto entra no if se equipe for vazio
-    if not equipe:
-        continuar = False
-        continue    #continua a execução do loop, testa e controla a variável continue
-    arquivo.write(equipe + '\n')
-arquivo.close()
+    while continuar:
+        equipe = input("Equipe (vazio para sair):")
+        #TODA STRING VAZIA É FALSE
+        #portanto entra no if se equipe for vazio
+        if not equipe:
+            continuar = False
+            continue    #continua a execução do loop, testa e controla a variável continue
+        arquivo.write(equipe + '\n')
+    arquivo.close()
+
+    teste = 10+"salve"    #testando o except do erro por divisão por 0
+
+except FileNotFoundError:
+    print("Arquivo ou diretório não encontrado")
+except ZeroDivisionError:
+    print("ta dividindo por 0 doente, faz o limite")
+except:
+    print("Algo deu bosta mano")
+finally:
+    print("agora ta suave")
